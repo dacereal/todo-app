abd21d3ad9a3502ac7161d40335bf7c22d2609d0
CARYL DAPANAS <dapanascaryl@gmail.com>
2024-09-25 14:47:01 +0800

    init project

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT ELECT</title>
+</head>
+<body>
+    <nav>
+        <a inref="/"> Home </a>
+        <a inref="/about"> About </a>
+    </nav>
+    {{ $slot }}
+</body>
+</html>
