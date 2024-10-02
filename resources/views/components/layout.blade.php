302b898480ac5cc31171f32769b9076f93b0b405
dacereal <dapanascaryl@gmail.com>
2024-10-02 12:15:45 +0800

    Activity - 1: Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IT ELECT</title>
+    <title>IT-ELECT</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
